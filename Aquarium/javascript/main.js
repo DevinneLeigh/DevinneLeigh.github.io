fcfc287e429013e1c9847cf0bc1c0233927b1234
Devinne Leigh <devinneleigh@gmail.com>
2024-09-11 23:17:05 -0600

    created memership and explore pages

--- Aquarium/javascript/main.js
+++ Aquarium/javascript/main.js
@@ -1,6 +1,6 @@
 
-let menuToggle = document.getElementById("menu_toggle")
-let mobileNav = document.getElementById("nav")
+const menuToggle = document.getElementById("menu_toggle")
+const mobileNav = document.getElementById("nav")
 
 menuToggle.addEventListener("click", function() {
     mobileNav.classList.toggle("open");
