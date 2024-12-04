754beb97ece8e6fc953bfd564ea39595a13bf8f0
Devinne Leigh <devinneleigh@gmail.com>
2024-12-03 23:12:48 -0700

    add 'dist' to base path

--- villagerdex/vite.config.js
+++ villagerdex/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://devinneleigh.github.io/villagerdex/",
+  base: "https://devinneleigh.github.io/villagerdex/dist/",
 })
